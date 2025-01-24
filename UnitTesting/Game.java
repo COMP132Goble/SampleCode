d4cc193c5404e0c5b7f23fe768e24a246fbdef55
wpgoble <will.goble@gmail.com>
2025-01-24 09:29:46 -0500

    updates code for testing purposes

--- UnitTesting/Game.java
+++ UnitTesting/Game.java
@@ -1,12 +1,30 @@
 package UnitTesting;
 
+import java.util.Random;
+
+/**
+ * A basic game that will leverage a few of the classes we build 
+ * throughout the semester
+ * 
+ * @author William Goble
+ */
 public class Game {
     public static void main(String[] args) {
-        Player player1, player2;
+        Player player1;
         player1 = new Player("Frodo");
-        player2 = new Player("Sam");
 
         player1.show();
-        player2.show();
+
+        // generate a random value between 0 and 100 as a double
+        Random r = new Random();
+        double minValue = 0.0;
+        double maxValue = 100.0;
+        double attackValue = minValue + (maxValue - minValue) * r.nextDouble();
+
+        // now let's attack Frodo and then let him take a long rest
+        player1.takeDameage(attackValue);
+        player1.show();
+        player1.longRest();
+        player1.show();
     }
 }
--- UnitTesting/Player.java
+++ UnitTesting/Player.java
@@ -1,5 +1,10 @@
 package UnitTesting;
 
+/**
+ * Basic player class that we will use throughout the semester
+ * 
+ * @author William Goble
+ */
 public class Player {
     String name;
     double health;
@@ -9,19 +14,34 @@ public class Player {
         health = 100;
     }
 
+    /**
+     * Quick method to see the player's health
+     */
     public void show() {
         System.out.println( name + " is at " + health + "% health");
     }
 
+    /**
+     * The player has been attacked, so let's remove some of their health
+     * @param amount
+     */
     public void takeDameage(double amount) {
-        health -= amount;
+        System.out.println("Ahh! " + name + " has been attacked!");
+        health += amount;
     }
 
+    /**
+     * After a long rest, the player is brought back to full health
+     */
     public void longRest() {
         health = 100;
     }
 
+    /**
+     * Heals the player by a given amount
+     * @param amount
+     */
     public void heal(double amount) {
-        health += amount;
+        health -= amount;
     }
 }
