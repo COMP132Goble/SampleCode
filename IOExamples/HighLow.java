b96f1d5483a75f58726d48af7519fd422f97a284
wpgoble <will.goble@gmail.com>
2025-02-07 10:00:14 -0500

    fixes skeleton for HighLow

--- IOExamples/HighLow.java
+++ IOExamples/HighLow.java
@@ -7,5 +7,9 @@ public class HighLow {
         Random generator = new Random();    // random number generator
         // generate a number between 1 and 100 inclusive
         int number = generator.nextInt(100) + 1;
+        for (int i = 0; i < 10; i++) {
+            System.out.println(number);
+            number = generator.nextInt(100) + 1;
+        }
     }
 }
