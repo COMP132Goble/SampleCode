a199820849cadf0470da5cae0742883ca65fbad7
wpgoble <will.goble@gmail.com>
2025-03-24 11:22:31 -0400

    implements selection sort

--- sorting/SelectionSort.java
+++ sorting/SelectionSort.java
@@ -2,15 +2,27 @@ package sorting;
 
 public class SelectionSort {
     public static int[] iterativeSort(int[] arr) {
-        return null;
+        for(int i = 0; i < arr.length; i++) {
+            int minIndex = findIndexOfMin(arr, i);
+            swap(arr, i, minIndex);
+        }
+        return arr;
     }
 
     private static int findIndexOfMin(int[] arr, int start) {
-        return 0;
+        int target = start;
+        for (int i = start + 1; i < arr.length; i++) {
+            if(arr[i] < arr[target]) {
+                target = i;
+            }
+        }
+        return target;
     }
 
     private static void swap(int[] arr, int val1, int val2) {
-        
+        int temp = arr[val1];
+        arr[val1] = arr[val2];
+        arr[val2] = temp;
     }
 
     public static void main(String[] args) {
@@ -22,7 +34,7 @@ public class SelectionSort {
 
     public static void testArrs(int[] arr) {
         ArrayTools.printIntArray(arr);
-        // arr = iterativeSort(arr);
-        // ArrayTools.printIntArray(arr);
+        arr = iterativeSort(arr);
+        ArrayTools.printIntArray(arr);
     }
 }
